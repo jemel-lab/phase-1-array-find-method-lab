ab43c3db4d336feb475730855b8b9f2f600d04de
jemel <jemel.defreitas@gmail.com>
2023-01-09 20:28:51 -0600

    commit tree 33437caeb5edf5d2c8279e265a7d8d1ef31604c8

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin (record) {
+   const win = record.find((rec) => rec.result ==='W');
+   if (win != null) { 
+    return win.year
+    
+   } else {
+    return undefined
+    
+   }
+}
+     
